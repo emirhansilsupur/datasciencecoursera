270efd3f20b4434883cedeaebcb864f1e8be895d
sremirhn <77552432+sremirhn@users.noreply.github.com>
2021-03-17 19:39:10 +0200

    deneme

--- first_d.R
+++ first_d.R
@@ -1,3 +1,5 @@
+#Holt's two parameter model,also known as linear exponential smoothing model
+#for forcesting data with trend.
 library(fpp2)
 Yt <- c(133,155,165,171,194,231,274,312,313,333,343)
 t<-1:11
